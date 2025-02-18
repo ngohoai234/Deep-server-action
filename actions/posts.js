2666c7c36c228c87b268927619f552a509611dec
ngohoai234 <cangod05@gmail.com>
2025-02-18 20:40:49 +0700

    chore: add server action

---
+++ actions/posts.js
@@ -0,0 +1,39 @@
+'use server';
+
+import { redirect } from 'next/navigation';
+
+export async function createPost(prevState, formData) {
+  'use server';
+  const title = formData.get('title');
+  const image = formData.get('image');
+  const content = formData.get('content');
+
+  const error = [];
+
+  if (!title || title.trim().length === 0) {
+    error.push('Title is required');
+  }
+
+  if (!content || content.trim().length === 0) {
+    error.push('Content is required');
+  }
+
+  if (!image || image.size === 0) {
+    error.push('Image is required');
+  }
+
+  if (error.length > 0) {
+    return {
+      errors: error,
+    };
+  }
+
+  await storePost({
+    imageUrl: '',
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect('/feed');
+}
